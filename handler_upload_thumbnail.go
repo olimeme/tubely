6f3a8eae1feff4b64cf731ff8873b861a2580456
olimeme <olisanmomyn@proton.me>
2026-05-27 18:06:48 +0500

    cache

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +32,78 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	const maxMemory = 10 << 20 
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form", err)
+		return
+	}
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get file from form", err)
+		return
+	}
+	defer file.Close()
 
-	// TODO: implement the upload here
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse content type", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Only jpeg or png files are accepted", nil)
+		return
+	}
+	
+	exts, _ := mime.ExtensionsByType(mediaType)
+	ext := ""
+	if len(exts) > 0 {
+		ext = exts[0]
+	}
+	if ext == "" {
+		ext = filepath.Ext(header.Filename)
+	}
+	if ext == "" {
+		respondWithError(w, http.StatusBadRequest, "Couldn't determine file extension", nil)
+		return
+	}
+
+	assetFilename := fmt.Sprintf("%s%s", videoID.String(), ext)
+	assetPath := filepath.Join(cfg.assetsRoot, assetFilename)
+
+	dst, err := os.Create(assetPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create asset file", err)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't save thumbnail file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not the owner of this video", nil)
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetFilename)
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, video)
 }
